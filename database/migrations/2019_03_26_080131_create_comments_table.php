415466e2bc9c896ac0da0a1d7878cc51b20ddbb7
HiroFujimoto <fhiro@soryu-sha.jp>
2019-03-26 19:10:33 +0900

    AUTHの改造。

--- database/migrations/2019_03_26_080131_create_comments_table.php
+++ database/migrations/2019_03_26_080131_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->bigInteger('author_id');
+            $table->bigInteger('comment_to');
+            $table->text('message');
+            $table->smallInteger('has_image')->default(0);
             $table->integer('status')->default(0);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
